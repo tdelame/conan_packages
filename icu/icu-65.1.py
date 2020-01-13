76dcd24e2cff2f8766ddbe3ee35b0a55f529d19c
Tom Delame <tdelame@gmail.com>
2020-01-13 09:54:59 +0100

    [Fix] do not remove pkg-config files in icu

--- icu/icu-65.1.py
+++ icu/icu-65.1.py
@@ -1,9 +1,10 @@
-from distutils.spawn import find_executable
-from shutil import rmtree
 import os
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+import shutil
+from glob import glob
+from conans import tools, python_requires
+pyreq = python_requires("pyreq/1.0.0@tdelame/stable")
 
-class ICU(ConanFile):
+class ICU(pyreq.BaseConanFile):
     description = "ICU is a mature, widely used set of C/C++ and Java libraries providing Unicode and Globalization support for software applications. ICU is widely portable and gives applications the same results on all platforms and between C/C++ and Java software"
     license = "ICU"
     url = "http://site.icu-project.org"
@@ -11,11 +12,6 @@ class ICU(ConanFile):
     name = "icu"
 
     settings = "os"
-    options = {"shared": [True, False]}
-    default_options = {"shared": True}
-
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         """Executed before the actual assignment of options. Use it to configure or constrain
@@ -32,32 +28,12 @@ class ICU(ConanFile):
 
     def build(self):
         """Build the elements to package."""
-        parallel = "-j{}".format(tools.cpu_count())
-
-        with tools.chdir(os.path.join(self._source_subfolder, "source")):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.fpic = True
-            if self.settings.os == "Linux" and find_executable("lld") is not None:
-                autotools.link_flags.append("-fuse-ld=lld")
-            autotools.flags = ["-O3"]
-            autotools.cxx_flags = ["-O3"]
-            autotools.configure(args=[
-                "--disable-fuzzer", "--disable-tests", "--disable-samples", "--disable-layoutex",
-                "--enable-release", "--disable-debug", "--with-library-bits=64",
-                "--enable-shared" if self.options.shared else "--disable-shared",
-                "--disable-static" if self.options.shared else "--enable-static"
-            ])
-            autotools.make(args=[parallel])
-            autotools.install(args=[parallel])
-
-    def package(self):
-        """Assemble the package."""
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder, ignore_case=True, keep_path=True)
-
-        # purge unneeded directories
-        rmtree(os.path.join(self.package_folder, "share"))
-        rmtree(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        rmtree(os.path.join(self.package_folder, "lib", "icu"))
+        arguments = [
+            "--disable-fuzzer", "--disable-tests", "--disable-samples", "--disable-layoutex",
+            "--enable-release", "--disable-debug", "--with-library-bits=64"
+        ]
+        
+        self.build_autotools(arguments, directory=os.path.join(self._source_subfolder, "source"))
 
     def package_info(self):
         """Edit package info."""
