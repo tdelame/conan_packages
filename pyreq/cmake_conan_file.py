ddd08cdeee945f341a804e02dcfcf8660113cdc3
Tom Delame <tdelame@gmail.com>
2020-01-15 16:31:06 +0100

    [Add] CMakeConanFile base class to pyreq

---
+++ pyreq/cmake_conan_file.py
@@ -0,0 +1,33 @@
+from base_conan_file import BaseConanFile
+from conans import CMake
+
+class CMakeConanFile(BaseConanFile):
+    
+    def build_requirements(self):
+        """Define build-time requirements."""
+        self.build_requires("cmake/3.15.4@tdelame/stable")
+        self.build_requires("ninja/1.9.0@tdelame/stable")
+
+    def cmake_definitions(self):
+        """Return a definition dict to be used by configure_cmake()."""
+        raise RuntimeError("You have to override 'cmake_definition()' in your derived class")
+
+    def configure_cmake(self):
+        """Configure and return a CMake build helper."""
+        cmake = CMake(self, generator="Ninja")
+        cmake.configure(
+            defs=self.cmake_definitions(),
+            source_folder=self._source_subfolder,
+            build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        """Build the elements to package."""
+        cmake = self.configure_cmake()
+        cmake.build()
+
+    def package(self):
+        """Assemble the package."""
+        cmake = self.configure_cmake()
+        cmake.install()
+        super(CMakeConanFile, self).package()
--- pyreq/pyreq-1.0.0.py
+++ pyreq/pyreq-1.0.0.py
@@ -2,6 +2,7 @@ from conans import ConanFile
 
 from utils import *
 from base_conan_file import *
+from cmake_conan_file import *
 from python_package import *
 
 
@@ -12,4 +13,4 @@ class pyreq(ConanFile):
 
     version = "1.0.0"
     name = "pyreq"
-    exports = ["utils.py", "base_conan_file.py", "python_package.py"]
+    exports = ["utils.py", "base_conan_file.py", "python_package.py", "cmake_conan_file.py"]
