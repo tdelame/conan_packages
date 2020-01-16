be90eb687468052fdc4e682d5db617fb12a86e9c
Tom Delame <tdelame@gmail.com>
2020-01-16 20:42:42 +0100

    [Add] boost-chrono 1.70.0

--- run.py
+++ run.py
@@ -131,6 +131,7 @@ def main():
     manager.manage("zstd", "1.4.4")                    # no deps
     manager.manage("double-conversion", "3.1.5")       # no deps
     manager.manage("libsndfile", "1.0.29")             # no deps
+    manager.manage("boost-chrono", "1.70.0")           # boost-headers
     manager.manage("embree", "3.3.0")                  # TBB
     manager.manage("flex", "2.6.4")                    # bison
     manager.manage("OpenSSL", "1.1.1d")                # zlib
