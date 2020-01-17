61118ac47de7499d815c02d55c168d319a16c419
Tom Delame <tdelame@gmail.com>
2020-01-17 15:58:44 +0100

    [Fix] fonstash recipe to use previously patched files

--- fontstash/fontstash-1.0.1.py
+++ fontstash/fontstash-1.0.1.py
@@ -8,22 +8,16 @@ class fontstash(ConanFile):
     license = "zlib"
     version = "1.0.1"
     name = "fontstash"
-    
-    def source(self):
-        """Retrieve source code."""
-        self.run("git clone {url} {destination}".format(
-            url=self.url,
-            destination=self.source_folder))
 
+    exports_sources = ["fontstash.h", "gl3fontstash.h", "glfontstash.h", "stb_truetype.h", "LICENSE.txt"]
+    
     def package(self):
         """Assemble the package."""
-        pyreq.make_directory("licenses")
-        pyreq.copy(
-            os.path.join(self.build_folder, "src"),
-            os.path.join(self.package_folder, "include"))
-        pyreq.copy(
-            os.path.join(self.build_folder, "LICENSE.txt"),
-            os.path.join(self.package_folder, "licenses", "LICENSE"))
+        self.copy("*.h", dst="include/fontstash")
+        self.copy("*.txt", dst="licenses")
         
+    def package_info(self):
+        self.cpp_info.includedirs = ["include"]
+
     def package_id(self):
         self.info.header_only()
