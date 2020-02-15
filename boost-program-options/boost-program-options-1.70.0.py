ac329942f5d546e3d5fb17c5680b509163101544
Tom Delame <tdelame@gmail.com>
2020-02-15 19:09:03 +0100

    [Add] boost program options 1.70.0

---
+++ boost-program-options/boost-program-options-1.70.0.py
@@ -0,0 +1,49 @@
+from conans import python_requires, tools
+import os
+pyreq = python_requires("pyreq/1.0.0@tdelame/stable")
+
+class boost_program_options(pyreq.BaseConanFile):
+    description = "Boost Program Options library"
+    license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
+    url = "https://www.boost.org/"
+    version = "1.70.0"
+    name = "boost-program-options"
+
+    settings = "os", "build_type"
+
+    def requirements(self):
+        self.requires("boost-headers/1.70.0@tdelame/stable")
+
+    def source(self):
+        """Retrieve source code."""
+        url = "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.gz"
+        tools.get(url)
+        os.rename("boost_1_70_0", self._source_subfolder)
+
+    def build(self):
+        """Build the elements to package."""
+
+        configure_command = "{bootstrap} --with-libraries={libraries_list}".format(
+            bootstrap="bootstrap" if os.name == "nt" else "./bootstrap.sh",
+            libraries_list="program_options")
+        
+        build_command = "{b2} stage --stagedir={stage} threading=multi link={link} variant={variant}".format(
+            b2="b2.exe" if os.name == "nt" else "./b2",
+            stage=self.package_folder,
+            link="shared" if self.options.shared else "static",
+            variant="release" if self.settings.build_type == "Release" else "debug")
+
+        with tools.chdir(os.path.join(self._source_subfolder)):
+            self.run(configure_command)
+            self.run(build_command)
+
+    def package(self):
+        """Assemble the package."""
+        self.copy("LICENSE_1_0.txt", dst="licenses", src=self._source_subfolder)
+
+    def package_info(self):
+        """Edit package info."""
+        super(boost_program_options, self).package_info()
+        self.cpp_info.libs = ["boost_program_options"]
+        self.cpp_info.system_libs = ["m", "pthread"]
+    
